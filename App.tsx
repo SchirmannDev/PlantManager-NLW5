a1f6879b1a71bdc25bf4b7b416588f34952451d6
schirmann <cschirmann.dev@gmail.com>
2021-04-19 17:02:13 -0300

    style: StatusBar inserida

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import { StatusBar } from 'react-native';
 import React from 'react';
 import { Welcome } from './src/pages/Welcome';
-
-
 
 export default function App() {
 
   return (
-    <Welcome />
+    <>
+      <Welcome />
+      <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
+    </>
   );
 
 }
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+
+import wateringImg from '../assets/watering.png';
 
 export function Welcome() {
 
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Text>
         Gerencie suas plantas de forma facil.
       </Text>
-    </View>
-  )
+      <Image source={wateringImg} />
+
+      <Text>
+        Não esqueça mais de regar suas plantas.
+        Nós cuidamos de lembrar você sempre que precisar.
+      </Text>
+
+      <TouchableOpacity>
+        <Text> > </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 20
+  }
+});
