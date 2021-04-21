93f0715a005ef11199866f4c02dc12e2a43f0b05
schirmann <cschirmann.dev@gmail.com>
2021-04-21 14:44:15 -0300

    style finalizado

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,35 +6,39 @@ import {
   TouchableOpacity,
   StyleSheet,
   Dimensions,
+  View,
 } from 'react-native';
 
 import { Feather } from '@expo/vector-icons';
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 import wateringImg from '../assets/watering.png';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma facil.
-      </Text>
-
-      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
-
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.{'\n'}
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
-
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
-        <Text>
-          <Feather name="chevron-right" style={styles.buttonIcon} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma facil.
         </Text>
-      </TouchableOpacity>
+
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.{'\n'}
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+          <Text>
+            <Feather name="chevron-right" style={styles.buttonIcon} />
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -42,22 +46,30 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
+
     paddingTop: 10,
   },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
   title: {
-    fontSize: 42,
+    fontSize: 38,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
     marginTop: 28,
+    fontFamily: fonts.heading,
+    lineHeight: 48,
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   button: {
     backgroundColor: colors.green,
