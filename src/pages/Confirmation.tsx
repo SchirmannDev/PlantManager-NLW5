30e29454fa68f6158232967ab55d193c1c6b83f4
schirmann <cschirmann.dev@gmail.com>
2021-04-21 17:42:09 -0300

    criando stylos

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -9,12 +9,48 @@ import { Button } from '../components/Button';
 export function Confirmation() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content} />
+      <View style={styles.content}>
+        <Text style={styles.emoji}>😁</Text>
+
+        <Text style={styles.title}>Prontinho!</Text>
+
+        <Text style={styles.subtitle}>
+          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <Button />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
   content: {},
+  emoji: {
+    fontSize: 32,
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 38,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 75,
+  },
 });
