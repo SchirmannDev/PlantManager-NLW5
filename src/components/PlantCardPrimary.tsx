9bd2dd36e8f6fcbbea7c4805ef7e5f371af1a60b
schirmann <cschirmann.dev@gmail.com>
2021-04-23 00:15:00 -0300

    svg inserido

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { SvgFromUri } from 'react-native-svg';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -16,6 +17,7 @@ interface PlantProps extends RectButtonProps {
 export const PlantCardPrimary = ({ data, ...rest }: PlantProps) => {
   return (
     <RectButton style={styles.container} {...rest}>
+      <SvgFromUri uri={data.photo} width={70} height={70} />
       <Text style={styles.text}>{data.name}</Text>
     </RectButton>
   );
