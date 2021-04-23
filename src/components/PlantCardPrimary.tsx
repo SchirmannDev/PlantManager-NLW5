be73a41ad0551e927d2d6dfbc279c36173302b67
schirmann <cschirmann.dev@gmail.com>
2021-04-22 21:39:45 -0300

    style concluido

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -3,6 +3,9 @@ import { StyleSheet, Text } from 'react-native';
 
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
 interface PlantProps extends RectButtonProps {
   data: {
     name: string;
@@ -13,12 +16,24 @@ interface PlantProps extends RectButtonProps {
 export const PlantCardPrimary = ({ data, ...rest }: PlantProps) => {
   return (
     <RectButton style={styles.container} {...rest}>
-      <Text style={styles.text}>{title}</Text>
+      <Text style={styles.text}>{data.name}</Text>
     </RectButton>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  text: {},
+  container: {
+    flex: 1,
+    maxWidth: '45%',
+    backgroundColor: colors.shape,
+    borderRadius: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+    margin: 10,
+  },
+  text: {
+    color: colors.green_dark,
+    fontFamily: fonts.heading,
+    marginVertical: 16,
+  },
 });
